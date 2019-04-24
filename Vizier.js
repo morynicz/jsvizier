8e96c75d1b9365a2b136573c954e48a5e878828d
Michał Orynicz <morynicz@gmail.com>
2019-04-24 10:53:50 +0200

    Added some new specs

--- Vizier.js
+++ Vizier.js
@@ -1,12 +1,19 @@
-function calcVizier(viziers, stages){
-  if (viziers.length == 0) {
+function calcVizier(viziers, stages) {
+  if (viziers.length == 0 || stages.length == 0) {
     return [];
   }
 
-  if (viziers[0] > stages[0]){
-    return [[viziers[0]]];
-  } else {
-    return [viziers];
-  }
+  let result = [];
 
+  result.push(viziers[0]);
+  if (viziers[0] > stages[0]) {
+    return [result];
+  } else {
+    for (let i = 1; i < viziers.length; i++) {
+      if (viziers[i] + result[0] > stages[0]) {
+        result.push(viziers[i]);
+        return [result];
+      }
+    }
+  }
 }
--- Vizier.spec.js
+++ Vizier.spec.js
@@ -1,22 +1,42 @@
-describe("detemines which viziers should be used on which stage of hunt", ()=>{
-  it("returns empty array when given empty inputs", ()=>{
-    expect(calcVizier([],[])).toEqual([]);
+describe("detemines which viziers should be used on which stage of hunt", () => {
+  it("returns empty array when viziers are empty", () => {
+    let viziers = [];
+    let stages = [2];
+    expect(calcVizier(viziers, stages)).toEqual([]);
+  });
+
+  it("returns empty array when stages are empty", () => {
+    let viziers = [1];
+    let stages = [];
+    expect(calcVizier(viziers, stages)).toEqual([]);
   });
 
   it("should return single vizier if his strength is not less than stage", () => {
-    expect(calcVizier([2],[1])).toEqual([[2]]);
+    expect(calcVizier([2], [1])).toEqual([[2]]);
   });
 
-  it("should return single vizier which is enough to pass the stage", ()=>{
+  it("should return single vizier which is enough to pass the stage", () => {
     let sufficientVizier = 2;
-    let viziers = [sufficientVizier,3];
+    let viziers = [sufficientVizier, 3];
     let stages = [1];
     expect(calcVizier(viziers, stages)).toEqual([[sufficientVizier]]);
   });
 
-  it("should return enough viziers to beat stage", ()=>{
-    let viziers = [1,2];
+  it("should return enough viziers to beat stage", () => {
+    let viziers = [1, 2];
     let stages = [2];
-    expect(calcVizier(viziers, stages)).toEqual([[1,2]]);
+    expect(calcVizier(viziers, stages)).toEqual([[1, 2]]);
+  });
+
+  it("should return not more viziers than required to beat stage", () => {
+    let viziers = [1, 2, 6];
+    let stages = [6];
+    expect(calcVizier(viziers, stages)).toEqual([[1, 6]]);
+  });
+
+  xit("should return vizier sequence for two stages", () => {
+    let viziers = [1, 2, 5, 10];
+    let stages = [6, 11];
+    expect(calcVizier(viziers, stages)).toEqual([[2, 5], [1, 10]]);
   });
 });
